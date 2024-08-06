22fe0c39de7508fc32d93fef53ede8b8e580c582
tairovpro <mamatysaevmukhammed@gmail.com>
2024-08-05 20:26:40 -0400

    Create WordWise_template.py
    Foundation file for main file of the web app

---
+++ WordWise_template.py
@@ -0,0 +1,22 @@
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+# Configure the MySQL database connection
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'mysql'
+app.config['MYSQL_DB'] = 'quiz-app'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+# Create a MySQL database connection
+mysql = pymysql.connect(
+    host=app.config['MYSQL_HOST'],
+    user=app.config['MYSQL_USER'],
+    password=app.config['MYSQL_PASSWORD'],
+    db=app.config['MYSQL_DB'],
+    cursorclass=pymysql.cursors.DictCursor
+)
+
+# Load questions from JSON file
+with open('questions.json', 'r') as file:
+    questions = json.load(file)
